4a8df8c7141c2cf24b40ce09002b0755ff6a7c0e
jshwi <stephen@jshwisolutions.com>
2020-05-31 16:06:07 +1000

    [refactor] Removes potential security vulnerabilities for requirements.txt

--- setup.py
+++ setup.py
@@ -35,5 +35,6 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     zip_safe=True,
-    install_requires=["pytest"],
+    install_requires=[],
+    python_requires=">=3.7",
 )
