3609a9012bc5d020f010f6f980af1f8b53785baf
jshwi <stephen@jshwisolutions.com>
2022-12-28 10:58:32 +1100

    test: Document params
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,5 +9,8 @@ from object_colors import Color
 
 @pytest.fixture(name="color")
 def fixture_color() -> Color:
-    """Instantiated ``Color`` object."""
+    """Instantiated ``Color`` object.
+
+    :return: Instantiated ``Color`` object.
+    """
     return Color()
