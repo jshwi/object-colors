e6bf431d187496500b4baaa65842e9ad398af283
jshwi <stephen@jshwisolutions.com>
2023-01-02 22:15:12 +1100

    chore: Update copyright year
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "objectcolors"
-copyright = "2022, Stephen Whitlock"
+project = "object-colors"
+copyright = "2023, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
